ad8aadb61eba1c9bbce3fac0c9730466207a10c1
Perfavore8 <nekit2353@mail.ru>
2022-08-26 18:11:28 +0200

    first commit

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/vueTestParadox/home/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vueTestParadox/" : "/",
 };
